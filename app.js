6feb9dda8cad353f5e8589704f0708840cc7f2d0
BeautyKehinde <137876817+BeautyKehinde@users.noreply.github.com>
2023-11-22 05:20:11 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const openAside= document.querySelector('.go')
+// const close=document.querySelector('.sedo')
+const openEl=function() {
+    openAside.className='sedo'
+}
+const closeEl=function() {
+    openAside.className='go'
+}
